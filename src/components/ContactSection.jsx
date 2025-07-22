134a1028c80ac4c77596b18c25a4756b22eb2aef
NagaSwaroop2611 <swaroopdayanandasagar@gmail.com>
2025-07-22 08:36:36 +0530

    Second Commit

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -35,7 +35,7 @@ export const ContactSection = () => {
   };
 
   return (
-    <section id="contact" className="py-24 px-4 relative bg-secondary/30 text-white">
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30 text-gradient">
       <div className="container mx-auto max-w-6xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
           Get In <span className="text-primary">Touch</span>
