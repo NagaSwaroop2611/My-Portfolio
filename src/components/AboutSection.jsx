463ea21e17f78426b0c52d30f5dd9d6a2f121047
NagaSwaroop2611 <swaroopdayanandasagar@gmail.com>
2025-07-22 08:23:48 +0530

    Second Coomit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ const AboutSection = () => {
               </a>
 
               <a
-                href="../assets/UpdatedResume.pdf"
+                href="https://drive.google.com/file/d/1FwUFW0RVIn00QgXu4KdZaW8aW9PcB5pW/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 target="_blank"
               >
